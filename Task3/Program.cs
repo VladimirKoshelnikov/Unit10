20334db0be64c376dbd553b3d9079a1310ac3b69
Кошельников Владимир <vkoshelnikov@bk.ru>
2023-07-18 14:25:27 +0300

    Unit 10

---
+++ Task3/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+
+namespace Task3{
+    class Program{       
+        class Subtask1{
+            public class Message{
+                string TextMessage;
+                string Address;
+
+            }
+
+            public interface IWriter{
+                void Write();
+            }
+        
+            public interface IReader{
+                void Read();
+            }
+            public interface IMailer{
+                void SendMail(Message message);
+            }
+
+            class FileManager :IWriter, IReader, IMailer{
+                void IWriter.Write(){
+                    throw new NotImplementedException();
+                }
+                void IReader.Read(){
+                    throw  new NotImplementedException();
+                }
+        
+                void IMailer.SendMail(Message message){
+                    throw  new NotImplementedException();
+                }
+            }
+        }
+        
+        class Subtask2{
+            public interface ICreatable 
+            {
+                void Create();
+            }
+
+            public interface IDeletable 
+            {
+                void Delete();
+            }
+
+            public interface IUpdatable 
+            {
+                void Update();
+            }
+
+
+            class Entity: ICreatable, IDeletable, IUpdatable{
+                public void Create(){
+                    throw  new NotImplementedException();
+                }
+                public void Delete(){
+                    throw  new NotImplementedException();
+                }
+                public void Update(){
+                    throw  new NotImplementedException();
+                }
+            }
+        }
+
+        class Subtask3{
+            public interface IBook 
+            {
+                public void Read();
+            }
+
+            public interface IDevice 
+            {
+                public void TurnOn();
+                public void TurnOff();
+            }
+            class ElectronicBook:  IBook, IDevice{
+  
+                void IBook.Read(){
+                    throw  new NotImplementedException();
+                }
+
+                void IDevice.TurnOn(){
+                    throw  new NotImplementedException();
+                }
+                void IDevice.TurnOff(){
+                    throw  new NotImplementedException();
+                }
+            }
+            public Subtask3(){
+                ElectronicBook eb = new ElectronicBook();
+                ((IBook)eb).Read();
+                ((IDevice)eb).TurnOn();
+                ((IDevice)eb).TurnOff();
+                
+                
+            }
+        }
+
+        static void Main(){
+
+            Console.WriteLine("Hello 3");
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.IO;
+
+namespace Task4{
+    class Program{
+        /*
+        Ковариантность позволяет использовать более конкретный тип, чем тип, который задан изначально.
+        Контравариантность позволяет использовать более универсальный тип, чем тип, который задан изначально.
+        */
+
+        class Subtask1{
+
+            /*
+            Ковариантность интерфейсов реализуется с помощью ключевого слова out
+
+            */
+
+            public interface IMessenger <out T>{
+                T DeviceInfo();
+            }
+
+            public class Viber<T>:IMessenger<T> where T:Phone, new(){
+                public T DeviceInfo(){
+                    T device = new T();
+                    Console.WriteLine(device);
+                    return new T();
+                }
+            }
+
+            public class Phone{}
+            public class IPhone:Phone{};
+            public class Computer{};
+
+            public Subtask1(){
+                IMessenger<Phone> viberInPhone = new Viber<Phone>();
+                IMessenger<Phone> viberInIPhone = new Viber<IPhone>();
+
+                viberInPhone.DeviceInfo();
+                viberInIPhone.DeviceInfo();
+
+            }
+        }
+
+        class Subtask2{
+            /*
+            Контрвариантность интерфейсов реализуется с помощью ключевого слова in
+            */
+
+            public interface IMessenger<in T>{
+                void GetDeviceInfo(T device);
+            }
+
+            public class Viber<T>:IMessenger<T> where T:Phone, new(){
+                public void GetDeviceInfo(T device){
+                    Console.WriteLine(device);
+                    }
+            }
+
+            public class Phone{}
+            public class IPhone:Phone{};
+            public class Computer{};
+
+            public Subtask2(){
+                IMessenger<Phone> viberInPhone = new Viber<Phone>();
+                viberInPhone.GetDeviceInfo(new Phone());
+
+                IMessenger<IPhone> viberInIphone = new Viber<Phone>();
+                viberInIphone.GetDeviceInfo(new IPhone());
+
+                Console.Read();    
+            }
+
+        }
+
+        class Subtask3 {
+            /*Один класс может быть как контрfвариантным, так и ковариантным*/
+            public class Car {
+
+            }
+            public class Bike:Car{
+
+            }
+
+            public class Home{
+
+            }
+            public class Garage:Home{
+
+            }
+
+            public interface IGarageManager<in T, out Z>{
+                public Z GetGarageInfo();
+                public void Add (T car);
+            }
+
+            public class GarageManagerBase:IGarageManager<Car, Garage>{
+                public void Add(Car car){
+                    throw new NotImplementedException();
+                }
+
+                public Garage GetGarageInfo(){
+                    throw new NotImplementedException();
+                }
+            }
+            public Subtask3(){
+                IGarageManager<Car, Garage> gmb1 = new GarageManagerBase();
+                IGarageManager<Bike, Garage> gmb2 = new GarageManagerBase();    
+                IGarageManager<Car, Home> gmb3 = new GarageManagerBase();
+            }
+        }
+
+        class Subtask4{
+
+            public class User 
+            {
+
+            }
+
+            public class Account: User 
+            {
+
+            }
+
+            public interface IUpdater< in T> 
+            {
+                void Update (T entity);
+            }
+
+            class UserService:IUpdater<User>{
+                public void Update(User entity){
+                    throw new NotImplementedException();
+                }
+            }
+
+            public Subtask4(){
+
+                Account account = new Account();
+                User user = new User();
+                
+                IUpdater<Account> updater = new UserService();
+                var userService = new UserService();
+                userService.Update(user);
+                
+                }
+        }
+
+        static void Main(){
+            Subtask1 st1 = new Subtask1();
+            Subtask2 st2 = new Subtask2();
+        }
+    }
+}

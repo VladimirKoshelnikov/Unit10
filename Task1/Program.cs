cdcb327bb87354ffabc68b82143535c76b9bd2f5
Кошельников Владимир <vkoshelnikov@bk.ru>
2023-07-18 14:25:17 +0300

    Unit 10

---
+++ Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Task1{
+    class Program{
+        class Subtask1{
+            public interface IWriter{
+                void Write(){ Console.WriteLine("Hello! I am Writer!");}
+            } 
+            public interface ILogger{
+                void Write(){ Console.WriteLine("Hello! I am Logger!");}
+            } 
+        
+            public class BusinessLogic : IWriter, ILogger{
+                
+            }
+        }
+
+        class Subtask2{
+                      
+            public class Manager : IManager
+            {
+                public void Create(){}
+
+                public void Read(){}
+
+                public void Update(){}
+
+                public void Delete(){}
+            }  
+
+            public interface IManager{
+                public void Create();
+                public void Read();
+                public void Update();
+                public void Delete();
+            }  
+        }
+
+        static void Main(){
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace Task2{
+    class Program{
+        
+        class Subtask1{
+            public interface IWhatsapp{
+                public void SendMessage(string message);
+            }
+
+            public interface IViber{
+                public void SendMessage(string message);
+            }
+
+            public class NewMessenger: IViber, IWhatsapp{
+                void IViber.SendMessage(string message){
+                    Console.WriteLine("{0}: {1}", "Viber", message);
+                }
+                void IWhatsapp.SendMessage(string message){
+                    Console.WriteLine("{0}: {1}", "WhatsApp", message);
+                }
+            }
+        
+            public Subtask1(){
+                NewMessenger newMessenger = new NewMessenger();
+
+                ((IWhatsapp)newMessenger).SendMessage("Hello Word");
+                ((IViber)newMessenger).SendMessage("Hello Word");
+
+                Console.ReadLine();
+            }
+        }
+
+        class Subtask2{
+            public interface IWriter{
+                public void Write(string message);
+            }
+
+            public class NewWriter: IWriter{
+                public void IWriter.Write(string message){
+                    throw NotImplementedException;
+                }
+            }
+        
+            public Subtask2(){
+                NewWriter writer = new NewWriter();
+                ((IWriter) writer).Write("Da");
+            }
+        }
+
+        class Subtask3{
+            public interface IWorker{
+                public void Build();
+            }
+
+            public class NewWorker: IWorker{
+                public void Build(){
+                    throw NotImplementedException;
+                }
+            }
+        
+            public Subtask3(){
+                NewWorker worker = new NewWorker();
+                ((IWorker) worker).Build();
+            }
+        }
+
+        static void Main(){
+            Subtask1 st1 = new Subtask1();
+        }
+    }
+}
